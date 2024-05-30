9f02a84b41ba2c9099d1c17d68eddfada18859c5
Karuna43 <76763022+Karuna43@users.noreply.github.com>
2024-05-30 19:07:20 +0530

    added resources

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "rg" {
+  description = "name of rg "
+  type      = string
+  default   = "rg_tech_challenge"
+}
+
+variable "location" {
+  description = "specify the location"
+  type     = string
+  default  = "eastus"
+}
+
+variable "vnet" {
+  description = "name of vent"
+  type      = string
+  default   = "vnet11"
+}
+
+variable "subnet" {
+  description = "name of subnet"
+  type    = string
+  default  = "subnet11"
+}
+
+variable "vm" {
+  description = "provide vm name"
+  type        = string
+  default     = "linux_vm"
+}
